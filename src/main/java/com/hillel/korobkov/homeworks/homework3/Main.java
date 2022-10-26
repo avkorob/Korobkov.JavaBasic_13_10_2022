ca9aa1e8a6674b2eb28ef845828441f5f36f8bca
Korobkov Alexander <av.korob@gmail.com>
2022-10-26 17:56:45 +0300

    homework3

---
+++ src/main/java/com/hillel/korobkov/homeworks/homework3/Main.java
@@ -0,0 +1,10 @@
+package com.hillel.korobkov.homeworks.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 50;
+        System.out.println(a);
+
+
+    }
+}

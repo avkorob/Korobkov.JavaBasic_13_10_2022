d98919953b7e3c4f758dcaec510ec36231fec264
Korobkov Alexander <av.korob@gmail.com>
2022-10-26 18:01:56 +0300

    homework3-1

---
+++ src/main/java/com/hillel/korobkov/homeworks/homework2/Main.java
@@ -0,0 +1,17 @@
+        package com.hillel.korobkov.homeworks.homework2;
+
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+        public class Main {
+            public static void main(String[] args) {
+                double n = 27.174931;
+                double e = 78.042097;
+
+                System.out.println("Taj Mahal:");
+                System.out.println("North-" + n + "°");
+                System.out.println("East-" + e + "°");
+
+
+            }
+        }
+

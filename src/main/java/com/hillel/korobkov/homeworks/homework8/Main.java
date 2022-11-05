07dff8625cd013c6d13a5cc067e70996362ac67d
Korobkov Alexander <av.korob@gmail.com>
2022-11-05 11:45:32 +0200

    homework 8-1

--- src/main/java/com/hillel/korobkov/homeworks/homework8/Main.java
+++ src/main/java/com/hillel/korobkov/homeworks/homework8/Main.java
@@ -2,6 +2,23 @@ package com.hillel.korobkov.homeworks.homework8;
 
 public class Main {
     public static void main(String[] args) {
+//        У Японії числа 4 та 9 вважаються нещасливими. Вам потрібно пронумерувати
+//        100 космічних шатлів для перевезення людей на Марс так, щоб в
+//        У номерах шатлів не траплялося нещасливих чисел. Напишіть функцію,
+//        яка виводить усі номери таких шатлів.
+
+        System.out.println("Номера шатлів для перевезення людей на Марс");
+        int counter = 0;
+        for (int i = 10; i <= 163; i++) {
+            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || i / 10 == 14) {
+                continue;
+            }
+            System.out.println("Номер шатлу: " + i);
+            counter++;
+        }
+
+        System.out.println(counter);
+
 
     }
 }

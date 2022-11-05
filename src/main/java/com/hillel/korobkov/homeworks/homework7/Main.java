836de55bed07af7fac3f61db97e426ea1a54d5f0
Korobkov Alexander <av.korob@gmail.com>
2022-11-05 14:54:05 +0200

    homework 8-1

--- src/main/java/com/hillel/korobkov/homeworks/homework7/Main.java
+++ src/main/java/com/hillel/korobkov/homeworks/homework7/Main.java
@@ -1,22 +1,59 @@
 package com.hillel.korobkov.homeworks.homework7;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//        У Японії числа 4 та 9 вважаються нещасливими. Вам потрібно пронумерувати
-//        100 космічних шатлів для перевезення людей на Марс так, щоб в
-//        У номерах шатлів не траплялося нещасливих чисел. Напишіть функцію,
-//        яка виводить усі номери таких шатлів.
 
-        int counter = 0;
-        for (int i = 10; i <= 153; i++) {
-            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9) {
-                continue;
+//        Зробити гру, в якій потрібно відгадати ціле число, яке "загадав" комп'ютер у діапазоні від 0 до 10
+//        Користувач повинен мати можливість вгадувати в кілька спроб (мінімум 3).
+
+        int userNumber, unknownNumber;
+        unknownNumber = (int) (Math.random() * 10) + 1;
+        System.out.println(unknownNumber);
+        System.out.println("Комп'ютер загадав число від 0 до 10! Спробуй вгадати!");
+        System.out.println("Введи число від 0 до 10! (У тебе є три спроби):");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            if (scanner.hasNextInt()) {
+                userNumber = scanner.nextInt();
+                if (userNumber == unknownNumber) {
+                    System.out.println("Вітаю! Ти вгадав, компь'ютер загадав число: " + unknownNumber);
+                    break;
+                } else {
+                    System.out.println("Ти не вгадав! У тебе залишилось 2 спроби! ");
+                }
+            } else {
+                System.out.println("Помилка! Необхідно число від 0 до 10!");
+                scanner.nextLine();
             }
-            System.out.println("shuttle number: " + i);
-            counter++;
+            if (scanner.hasNextInt()) {
+                userNumber = scanner.nextInt();
+                if (userNumber == unknownNumber) {
+                    System.out.println("Вітаю! Ти вгадав, компь'ютер загадав число: " + unknownNumber);
+                    break;
+                } else {
+                    System.out.println("Ти не вгадав! У тебе залишилось 1 спроба! ");
+                }
+            } else {
+                System.out.println("Помилка! Необхідно число від 0 до 10!");
+                scanner.nextLine();
+            }
+            if (scanner.hasNextInt()) {
+                userNumber = scanner.nextInt();
+                if (userNumber == unknownNumber) {
+                    System.out.println("Вітаю! Ти вгадав, компь'ютер загадав число: " + unknownNumber);
+                    break;
+                } else {
+                    System.out.println("Ти не вгадав! Спробуй ще!");
+                }
+                break;
+            } else {
+                System.out.println("Помилка! Необхідно число від 0 до 10!");
+                scanner.nextLine();
+            }
         }
-
-        System.out.println(counter);
-
     }
 }
